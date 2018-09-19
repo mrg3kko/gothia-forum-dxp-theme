f69e78579676bcb52ed086f97d1e642a400bfb0b
mrg3kko <erik.andersson@monator.com>
2018-09-19 10:50:34 +0200

    Added faq js

--- src/js/jq.js
+++ src/js/jq.js
@@ -23,3 +23,44 @@
   });
 
 })( jQuery, window, document );
+
+
+
+
+//FAQ
+;( function( $, window, document, undefined ) {
+  'use strict';
+
+  var faq = $('.faq');
+  faq.addClass('faq-js');
+
+  var entryQuestions = $('.faq dt');
+  var entryAnswers = $('.faq dd');
+
+  // Add raquo to each question
+  entryQuestions.each(function(index, node) {
+    var html = $(node).html();
+    var html = html + ' &raquo;';
+    $(node).html(html);
+  });
+
+  // Hide all answers
+  entryAnswers.hide();
+
+  // Bind click callback to questions
+  entryQuestions.click(function(e) {
+    var entryNode = $(e.currentTarget);
+    var answerNode = entryNode.next();
+
+    if(answerNode) {
+      if(answerNode.css('display') == 'none') {
+        console.log('answerNode has class hide');
+        answerNode.show();
+      } else {
+        answerNode.hide();
+      }
+    }
+
+  });
+
+})( jQuery, window, document );
